4ba0cd25df5c00c79a8a4b805b18aa979d4e1dcd
imvichou <77609971+imvichou@users.noreply.github.com>
2021-02-26 02:07:14 +0800

    20210226-02

--- MachineLearning/Form1.cs
+++ MachineLearning/Form1.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Microsoft.ML;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,30 @@ namespace MachineLearning
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var context = new MLContext();
+
+            var data = context.Data.LoadFromTextFile<HousingData>("./Sources/housing.csv", hasHeader: true, separatorChar: ',');
+
+            var split = context.Data.TrainTestSplit(data, testFraction: 0.2);
+
+            var features = split.TrainSet.Schema.Select(col => col.Name).Where(colName => colName != "Label" && colName != "OceanProximity").ToArray();
+
+            var pipeline = context.Transforms.Text.FeaturizeText("Text", "OceanProximity")
+                .Append(context.Transforms.Concatenate("Features", features))
+                .Append(context.Transforms.Concatenate("Features", "Features", "Text"))
+                .Append(context.Regression.Trainers.LbfgsPoissonRegression());
+
+            var model = pipeline.Fit(split.TrainSet);
+
+            var predictions = model.Transform(split.TestSet);
+
+            var metrics = context.Regression.Evaluate(predictions);
+
+            Debug.WriteLine($"R^2- {metrics.RSquared}");
         }
     }
 }
